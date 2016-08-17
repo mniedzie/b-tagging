46bedabcb1f92feaf0c8c7d5226ef4097710e737
marek niedziela <marek.niedziela@desy.de>
2016-08-17 14:56:58 +0200

    exercises

---
+++ test/runBTaggingExerciseII_cfg.py
@@ -0,0 +1,88 @@
+import FWCore.ParameterSet.Config as cms
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+###############################
+####### Parameters ############
+###############################
+
+options = VarParsing ('python')
+
+options.register('reportEvery', 10,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.int,
+    "Report every N events (default is N=10)"
+)
+options.register('outputFilename', 'exerciseII_histos.root',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Output file name"
+)
+options.register('process', 'ttbar',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "MC-simulated event type"
+)
+options.register('wantSummary', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Print out trigger and timing summary"
+)
+
+## 'maxEvents' is already registered by the Framework, changing default value
+options.setDefault('maxEvents', 100)
+
+options.parseArguments()
+
+process = cms.Process("USER")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+
+## Events to process
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+
+## Input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        # /TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM
+		  '/store/mc/RunIISpring16MiniAODv2/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/10000/00D85087-7925-E611-BE0A-6C3BE5B59200.root'
+    )
+)
+
+if options.process == "QCD":
+    process.source.fileNames = [
+        # /QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_magnetOn_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM
+        #'/store/mc/RunIIFall15MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/PU25nsData2015v1_magnetOn_76X_mcRun2_asymptotic_v12-v1/00000/08533D46-4BBC-E511-84AF-B083FEC76567.root'
+		  '/store/mc/RunIISpring16MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/NoPU_PUSpring16Fast_ForSUSJECs_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/10000/021274A0-2434-E611-82CA-001E67460991.root'
+    ]
+
+## Output file
+process.TFileService = cms.Service("TFileService",
+   fileName = cms.string(options.outputFilename.replace('.root','_' + options.process + '.root'))
+)
+
+## Options and Output Report
+process.options   = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(options.wantSummary),
+    allowUnscheduled = cms.untracked.bool(True)
+)
+getattr(process,'slimmedJets').addTagInfos = cms.bool(True)
+## Initialize analyzer
+process.bTaggingExerciseII = cms.EDAnalyzer('BTaggingExerciseII',
+    jets = cms.InputTag('slimmedJets'), # input jet collection name
+    bDiscriminators = cms.vstring(      # list of b-tag discriminators to access
+        'pfTrackCountingHighEffBJetTags',
+        'pfTrackCountingHighPurBJetTags',
+        'pfJetProbabilityBJetTags',
+        'pfJetBProbabilityBJetTags',
+        'pfSimpleSecondaryVertexHighEffBJetTags',
+        'pfSimpleSecondaryVertexHighPurBJetTags',
+        'pfCombinedSecondaryVertexV2BJetTags',
+        'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+        'pfCombinedMVABJetTags'
+    )
+)
+
+## Let it run
+process.p = cms.Path(process.bTaggingExerciseII)
